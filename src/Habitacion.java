3c6925bb3899b5266ce7c3ceadd7cad398e96cfb
LeonardoMero98 <erick.mero@udla.edu.ec>
2025-05-26 12:23:46 -0500

    Estructura inicial del proyecto y clase Hotel implementada

---
+++ src/Habitacion.java
@@ -0,0 +1,15 @@
+/**
+ * Clase Habitacion - Placeholder temporal
+ * Será implementada por Paola Maldonado
+ */
+public class Habitacion {
+    private int numero;
+
+    public Habitacion(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+}
